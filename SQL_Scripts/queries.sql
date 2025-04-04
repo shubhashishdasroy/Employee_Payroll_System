cbcf9fe57c5afe15fb494cf44a3c5713915aa3c2
shubhashishdasroy <sdr.23cs8013@nitdgp.ac.in>
2025-04-05 00:59:30 +0530

    Update queries.sql

---
+++ SQL_Scripts/queries.sql
@@ -0,0 +1,48 @@
+-- Q1. Retrieve all employees and their departments
+SELECT Name,DepartmentName 
+FROM Employees Natural Join Departments;
+
+-- Q2. Calculate total salary paid per department
+
+
+-- Q3. List employees earning above a certain salary (e.g., 60000)
+
+
+-- Q4. Update an employee’s salary (e.g., increase salary of EmployeeID 103 to 80000)
+
+
+-- Q5. Find the highest-paid employee
+
+
+-- Q6. Delete an employee record (e.g., delete EmployeeID 110)
+
+
+-- Q7. Retrieve employees who have been with the company for more than 5 years
+
+
+-- Q8. Find employees with salaries above the department average
+
+
+-- Q9. Retrieve employees who received a salary increase in the last year
+
+
+-- Q10. List departments with the highest total salary expenses
+
+
+-- Q11. Find the most common salary amount in the payroll
+
+
+-- Q12. Retrieve employees sorted by salary in descending order
+
+
+-- Q13. Find employees whose salaries increased by more than 10% in the last year
+
+
+-- Q14. Retrieve payroll records for a specific month (e.g., 'February')
+
+
+-- Q15. Find employees who received bonuses above a certain amount (Assuming a "Bonus" field exists)
+
+
+-- Q16. List employees with the lowest salaries in each department
+
