8437c1a54ae9bf530f3ae9d835868f5523ee9782
shubhashishdasroy <sdr.23cs8013@nitdgp.ac.in>
2025-04-05 23:00:01 +0530

    Update create_table.sql

--- SQL_Scripts/create_table.sql
+++ SQL_Scripts/create_table.sql
@@ -1,26 +1,24 @@
-CREATE DATABASE employee_payroll;
-USE employee_payroll;
-
 CREATE TABLE Departments (
-    DepartmentID NUMBER PRIMARY KEY,
-    DepartmentName VARCHAR2(50) NOT NULL
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(100) NOT NULL UNIQUE
 );
 
-
 CREATE TABLE Employees (
-    EmployeeID NUMBER PRIMARY KEY,
-    Name VARCHAR2(50) NOT NULL,
-    Age NUMBER,
-    Salary NUMBER(10, 2),
-    DepartmentID NUMBER,
+    EmployeeID INT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Age INT CHECK (Age >= 18),
+    Salary DECIMAL(10, 2),
+    DepartmentID INT,
+    JoinDate DATE,
     FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
 );
 
 CREATE TABLE Payroll (
-    PayrollID NUMBER PRIMARY KEY,
-    EmployeeID NUMBER,
-    SalaryMonth VARCHAR2(20),
-    SalaryAmount NUMBER(10,2),
-    DepartmentID NUMBER,
+    PayrollID INT PRIMARY KEY,
+    EmployeeID INT,
+    SalaryMonth VARCHAR(20) NOT NULL,
+    SalaryAmount DECIMAL(10, 2),
+    Bonus DECIMAL(10, 2) DEFAULT 0,
     FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID)
 );
+
